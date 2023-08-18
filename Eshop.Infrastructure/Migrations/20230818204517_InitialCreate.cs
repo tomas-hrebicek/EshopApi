96b9e1c8a565435d3fbeb3bc1908959a2a7648fe
Tomáš Hřebíček <88036219+tomas-hrebicek@users.noreply.github.com>
2023-08-18 23:05:48 +0200

    Migration InitialCreate

---
+++ Eshop.Infrastructure/Migrations/20230818204517_InitialCreate.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Eshop.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImgUri = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,4)", precision: 18, scale: 4, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product_Id", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImgUri", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, null, "https://rohlik.img", "Rohlík", 3.5m },
+                    { 2, null, "https://houska.img", "Houska", 3.5m },
+                    { 3, "houskový knedlík", "https://knedlik.img", "Knedlík", 24.9m },
+                    { 4, null, "https://vanocka.img", "Vánočka", 49.9m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
