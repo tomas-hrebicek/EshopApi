d83ed3ec201f6c922961fa6b7d8bfa92fddeb21d
Tomáš Hřebíček <88036219+tomas-hrebicek@users.noreply.github.com>
2024-02-06 22:27:02 +0100

    creating extensions for simply configuration in Program.cs

--- Sample.Api/Program.cs
+++ Sample.Api/Program.cs
@@ -1,74 +1,21 @@
-using Asp.Versioning;
-using Asp.Versioning.ApiExplorer;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 using Sample.Api;
 using Sample.Application;
 using Sample.Infrastructure;
-using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Logging.AddConfiguration(builder.Configuration).AddConsole();
 
 builder.Services.AddTokenAuthentication(builder.Configuration);
 builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString("Default"));
 builder.Services.AddApplicationLayer();
 
-builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
-{
-    options.InvalidModelStateResponseFactory = actionContext =>
-    {
-        return new BadRequestObjectResult(new BadRequestApiError(actionContext));
-    };
-});
+builder.Services.AddValidatedControllers();
+builder.Services.AddVersioning();
 
-builder.Services.AddApiVersioning(options =>
-{
-    options.DefaultApiVersion = new ApiVersion(1, 0);
-    options.AssumeDefaultVersionWhenUnspecified = true;
-    options.ReportApiVersions = true;
-}).AddApiExplorer(options =>
-{
-    options.GroupNameFormat = "'v'VVV";
-    options.SubstituteApiVersionInUrl = true;
-});
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{ 
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"), false);
-    options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Description = "Please enter a valid token",
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        BearerFormat = "JWT",
-        Scheme = JwtBearerDefaults.AuthenticationScheme
-    });
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = JwtBearerDefaults.AuthenticationScheme
-                }
-            },
-            new string[] { }
-        }
-    });
-});
-
-builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
+builder.Services.AddSwagger();
 
 var app = builder.Build();
 
@@ -84,21 +31,11 @@ else
 
 app.UseMiddleware<SampleApiExceptionHandlerMiddleware>();
 
-app.UseSwagger();
-app.UseSwaggerUI(
-        options =>
-        {
-            var provider = app.Services.GetService<IApiVersionDescriptionProvider>();
-            // build a swagger endpoint for each discovered API version
-            foreach (var description in provider.ApiVersionDescriptions)
-            {
-                options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName);
-            }
-        });
+app.UseSwaggerVersionedUI(app.Services);
 
 app.UseHttpsRedirection();
 
-app.UseCors(builder.Configuration);
+app.UseCors(app.Configuration);
 
 app.UseAuthentication();
 app.UseAuthorization();
---
+++ Sample.Api/ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace Sample.Api
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IApiVersioningBuilder AddVersioning(this IServiceCollection services)
+        {
+            return services.AddApiVersioning(options =>
+            {
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ReportApiVersions = true;
+            }).AddApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+        }
+
+        public static IMvcBuilder AddValidatedControllers(this IServiceCollection services)
+        {
+            return services.AddControllers().ConfigureApiBehaviorOptions(options =>
+            {
+                options.InvalidModelStateResponseFactory = actionContext =>
+                {
+                    return new BadRequestObjectResult(new BadRequestApiError(actionContext));
+                };
+            });
+        }
+    }
+}
