2343dd53593149be702657fdf9cd9631894f1dad
Tomáš Hřebíček <88036219+tomas-hrebicek@users.noreply.github.com>
2024-02-06 22:24:15 +0100

    move swagger configuration to extension classes

---
+++ Sample.Api/SwaggerBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Asp.Versioning.ApiExplorer;
+
+namespace Sample.Api
+{
+    public static class SwaggerBuilderExtensions
+    {
+        public static IApplicationBuilder UseSwaggerVersionedUI(this IApplicationBuilder app, IServiceProvider services)
+        {
+            app.UseSwagger();
+            return app.UseSwaggerUI(
+                    options =>
+                    {
+                        var provider = services.GetService<IApiVersionDescriptionProvider>();
+                        // build a swagger endpoint for each discovered API version
+                        foreach (var description in provider.ApiVersionDescriptions)
+                        {
+                            options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName);
+                        }
+                    });
+        }
+    }
+}
