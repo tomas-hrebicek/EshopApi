16cee828295449fdc34ebb03ace74ea1e579fc7a
Tomáš Hřebíček <88036219+tomas-hrebicek@users.noreply.github.com>
2024-09-21 23:10:31 +0200

    [ADD] IsoDateTime System.Text.Json Converter

---
+++ Sample.Api/IsoDateTimeConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace Sample.Api
+{
+    internal sealed class IsoDateTimeConverter : JsonConverter<DateTime>
+    {
+        private const string ISODateTimeFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss.FFFFFFFK";
+
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (DateTime.TryParseExact(reader.GetString(), ISODateTimeFormat, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal, out DateTime dateTime))
+            {
+                return dateTime;
+            }
+            else
+            {
+                throw new JsonException("Invalid date format");
+            }
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString(ISODateTimeFormat));
+        }
+    }
+}
